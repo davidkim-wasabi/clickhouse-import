74151945a26fb3d0f39cd1a202df783c426e3a7c
David Kim <jk2537@cornell.edu>
2020-08-20 12:23:36 -0400

    Load tables in bulk script

---
+++ loadDB_bulk.py
@@ -0,0 +1,24 @@
+import clickhouse_secrets as chcreds
+from clickhouse_driver import Client
+import sys
+import os
+
+client = Client("localhost", user=chcreds.user, password=chcreds.password)
+
+base_path = os.path.join("/home", "users", "dkim", "mysql_data_transfer")
+global_tables = os.listdir(os.path.join(base_path, "BA_Global"))
+billing_tables = os.listdir(os.path.join(base_path, "BA_Billing"))
+
+for tbl in global_tables:
+  if ".csv" in tbl:
+    os.system('cat {}.csv | \
+        docker exec -i clickhouse-server clickhouse-client --user admin \
+        --password 4irehose --query="INSERT INTO BA_Global.{} FORMAT CSV"'.format(
+        os.path.splitext(tbl)[0]))
+
+for tbl in billing_tables:
+  if ".csv" in tbl:
+    os.system('cat {}.csv | \
+        docker exec -i clickhouse-server clickhouse-client --user admin \
+        --password 4irehose --query="INSERT INTO BA_Billing.{} FORMAT CSV"'.format(
+        os.path.splitext(tbl)[0]))
