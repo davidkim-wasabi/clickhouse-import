07a10263f0ea627a5f8a0d89f08147ab3ca05fe1
David Kim <jk2537@cornell.edu>
2020-08-20 11:46:11 -0400

    bugfix

--- createDB_bulk.py
+++ createDB_bulk.py
@@ -1,10 +1,11 @@
 import clickhouse_secrets as chcreds
 from clickhouse_driver import Client
 import sys
+import os
 
 client = Client("localhost", user=chcreds.user, password=chcreds.password)
 
-base_path = os.path.join("home", "users", "dkim", "mysql_data_transfer", "GDB_dbstarter")
+base_path = os.path.join("/home", "users", "dkim", "mysql_data_transfer", "GDB_dbstarter")
 global_tables = os.listdir(os.path.join(base_path, "BA_Global"))
 billing_tables = os.listdir(os.path.join(base_path, "BA_Billing"))
 
@@ -14,7 +15,7 @@ client.execute("CREATE DATABASE IF NOT EXISTS BA_Billing")
 
 for tbl in global_tables:
   if ".txt" in tbl:
-    content = open(tbl, "r").read()
+    content = open(os.path.join(base_path, "BA_Global", tbl), "r").read()
     query = content.replace("\n", " ")
 
     table_name = os.path.splitext(tbl)[0]
@@ -24,7 +25,7 @@ for tbl in global_tables:
 
 for tbl in billing_tables:
   if ".txt" in tbl:
-    content = open(tbl, "r").read()
+    content = open(os.path.join(base_path, "BA_Billing", tbl), "r").read()
     query = content.replace("\n", " ")
 
     table_name = os.path.splitext(tbl)[0]
